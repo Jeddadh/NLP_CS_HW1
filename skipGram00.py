192c1201853f7dd0c14dc15f29baa891120d27bb
Hamza JEDDAD <hamza.jeddad@student.ecp.fr>
2019-02-16 17:55:03 +0100

    updated negative sampling

--- skipGram00.py
+++ skipGram00.py
@@ -36,7 +36,11 @@ def loadPairs(path):
     pairs = zip(data['word1'],data['word2'],data['similarity'])
     return pairs
 
-def sigmoid(z):
+def sigmoid(z,limit_sup=12):
+    if -limit_sup > z  :
+        return 0
+    if z > limit_sup  :
+        return 1
     return 1/(1+np.exp(-z))
 
 def derivate_sigmoid(z):
@@ -86,7 +90,7 @@ class SkipGram():
         self.vocab_index = {}
         self.__create_vocabulary()
         self.__initialize_embeddings()
-        self.__create_negative_samples()
+        # self.__create_negative_samples()
 
     def __create_vocabulary(self):
         print("creating vocab")
@@ -103,11 +107,15 @@ class SkipGram():
         word_to_be_eliminated = []
         index = 0
         self.nb_all_words = 0
+        self.nnnnn = 0
         for word in self.vocab :
             if self.vocab[word] <= self.minCount :
                 word_to_be_eliminated.append(word)
             else :
+                self.nnnnn += self.vocab[word]
+                self.vocab[word] = self.vocab[word]**0.75 #for the unigram distribution computation
                 self.nb_all_words += self.vocab[word]
+
                 self.vocab_index[word] = index
                 index += 1
         for word in word_to_be_eliminated :
@@ -119,15 +127,27 @@ class SkipGram():
 
         # create contexts
         print("creating context")
+        proba_list = list(self.vocab.values())
+        nnnnn = len(self.vocab)
+        iiiii = 0
         for sentence in self.sentences :
             for i,word in enumerate(sentence) :
+                if not i % 10 :
+                    print("word {i}/{t}".format(i=int(100*(iiiii/self.nnnnn)),t=100),end="\r")
+
                 if word not in self.vocab_index :
                     continue
                 sentence_size = len(sentence)
+                iiiii += 1
                 for j in self.contexts_indices :
                     if 0 <= i + j < sentence_size :
                         try :
                             self.set_contexts[self.vocab_index[word]].add(self.vocab_index[sentence[i+j]])
+                            self.set_negative_samples[self.vocab_index[word]] = self.set_negative_samples.get(self.vocab_index[word],set())
+                            # print(np.random.choice(list(self.vocab),size=self.negativeRate,p=proba_list))
+                            self.set_negative_samples[self.vocab_index[word]] = self.set_negative_samples[self.vocab_index[word]].union(set(np.random.choice(list(self.vocab_index.values()),size=self.negativeRate,p=proba_list)))
+                            self.set_negative_samples[self.vocab_index[word]].remove(word)
+
                         except KeyError :
                             # the word is not in vocab_index
                             pass
@@ -150,20 +170,20 @@ class SkipGram():
             self.W_context_gradients[word] = np.zeros(self.nEmbed).astype('float64')
 
 
-    def __create_negative_samples(self):
-        print("creating negative samples")
-        self.set_negative_samples = {i:set() for i in self.vocab_index.values()}
-        # return None
-        for word in self.set_contexts:
-            nb_negative_samples = 0
-            possible_words = set(self.vocab_index.values()).difference(self.set_contexts[word])
-            n_desired_neg_samples = self.negativeRate*len(self.set_contexts[word])
-            nb_possible_words = len(possible_words)
-            try :
-                possible_words.remove(word)
-            except KeyError:
-                pass
-            self.set_negative_samples[word] = set(np.random.choice(list(possible_words),size=min(n_desired_neg_samples,nb_possible_words)))
+    # def __create_negative_samples(self):
+    #     print("creating negative samples")
+    #     self.set_negative_samples = {i:set() for i in self.vocab_index.values()}
+    #     # return None
+    #     for word in self.set_contexts:
+    #         nb_negative_samples = 0
+    #         possible_words = set(self.vocab_index.values()).difference(self.set_contexts[word])
+    #         n_desired_neg_samples = self.negativeRate*len(self.set_contexts[word])
+    #         nb_possible_words = len(possible_words)
+    #         try :
+    #             possible_words.remove(word)
+    #         except KeyError:
+    #             pass
+    #         self.set_negative_samples[word] = set(np.random.choice(list(possible_words),size=min(n_desired_neg_samples,nb_possible_words)))
 
     def __loss(self):
         loss = 0
@@ -183,18 +203,21 @@ class SkipGram():
                 contexts_size = int(batch_percentage*len(self.set_contexts[word]))
                 negative_contexts_size = contexts_size = int(batch_percentage*len(self.set_negative_samples[word]))
                 contexts = np.random.choice(list(self.set_contexts[word]),contexts_size)
+                # print(self.set_negative_samples[word])
                 negative_contexts = np.random.choice(list(self.set_negative_samples[word]),negative_contexts_size)
             else :
                 contexts = self.set_contexts[word]
                 negative_contexts = self.set_negative_samples[word]
             for context_word in contexts:
+                # Sum on D
                 dot_product = np.dot(self.W_embedding[word],self.W_context[context_word])
-                neg_exp_exp = safe_exp_exp(-dot_product)
+                neg_exp_exp = sigmoid(-dot_product)
                 self.W_embedding_gradient[word] = self.W_embedding_gradient[word] - neg_exp_exp*self.W_context[context_word]
                 self.W_context_gradients[context_word] = self.W_context_gradients[context_word] - neg_exp_exp*self.W_embedding[word]
             for context_word in negative_contexts:
+                # Sum on D'
                 dot_product = np.dot(self.W_embedding[word],self.W_context[context_word])
-                pos_exp_exp = safe_exp_exp(dot_product)
+                pos_exp_exp = sigmoid(dot_product)
                 self.W_embedding_gradient[word] = self.W_embedding_gradient[word] + pos_exp_exp*self.W_context[context_word]
                 self.W_context_gradients[context_word] = self.W_context_gradients[context_word] + pos_exp_exp*self.W_embedding[word]
         # update the params
@@ -218,16 +241,22 @@ class SkipGram():
     def similarity(self,word1,word2):
         """
             computes similiarity between the two words. unknown words are mapped to one common vector
+            We choose the null vector as the one common vector
         :param word1:
         :param word2:
         :return: a float \in [0,1] indicating the similarity (the higher the more similar)
+
         """
-        if (word1 not in self.vocab_index.keys()) or (word2 not in self.vocab_index.keys()):
+
+        if word1 not in self.vocab_index :
             return 0
-        else:
-            emb1 = self.W_embedding[self.vocab_index[word1]]
-            emb2 = self.W_embedding[self.vocab_index[word2]]
-            return 0.5+0.5*np.dot(emb1,emb2)/(np.linalg.norm(emb1)*np.linalg.norm(emb2))
+
+        if  word2 not in self.vocab_index:
+            return 0
+
+        emb1 = self.W_embedding[self.vocab_index[word1]]
+        emb2 = self.W_embedding[self.vocab_index[word2]]
+        return 0.5+0.5*np.dot(emb1,emb2)/(np.linalg.norm(emb1)*np.linalg.norm(emb2))
 
 
 
@@ -247,6 +276,7 @@ if __name__ == '__main__':
     # if not opts.test:
 
     path = "C:/Users/Hamza/Centrale3A/MscAI_2018_2019/NLP/DM1/1-billion-word-language-modeling-benchmark-r13output/training-monolingual.tokenized.shuffled/news.en-00001-of-00100"
+    path =     "C:/Users/Hamza/Centrale3A/MscAI_2018_2019/NLP/DM1/1-billion-word-language-modeling-benchmark-r13output/heldout-monolingual.tokenized.shuffled/news.en.heldout-00000-of-00050"
     # path = "C:/Users/Hamza/Centrale3A/MscAI_2018_2019/NLP/DM1/my_data.txt"
     print("preparing sentences")
     sentences = text2sentences(path)
